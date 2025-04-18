0e62c0edc3c71ebe34512ada9b9570ca810fd35c
CrazyMikha2010 <mikhail.mezh@icloud.com>
2025-04-18 17:57:24 +0300

    added text wrapper, set custom fonts

--- settings.py
+++ settings.py
@@ -12,7 +12,23 @@ FPS = 60
 w, h = 1080, 720
 scr = pg.display.set_mode((w, h))
 
-font_xl = pg.font.SysFont('Comic Sans MS', 70)
-font_l = pg.font.SysFont('Comic Sans MS', 40)
-font_m = pg.font.SysFont('Comic Sans MS', 30)
-font_s = pg.font.SysFont('Comic Sans MS', 20)
+font_path = mainpath + "/Dp_font-Regular 3.ttf"
+font_m = pg.font.Font(font_path, 60)
+font_xl_num = pg.font.SysFont("Comic Sans", 70)
+font_xl = pg.font.Font(font_path, 140)
+font_l = pg.font.Font(font_path, 80)
+font_s = pg.font.Font(font_path, 40)
+
+class Text_Wrapper:
+    def __init__(self):
+        ...
+
+    def text_wrapper(self, text, font, width):
+        lines = [[]]
+        for word in text.split(' '):
+            test_line = ' '.join(lines[-1] + [word])
+            if font.size(test_line)[0] < width:
+                lines[-1].append(word)
+            else:
+                lines.append([word])
+        return lines
